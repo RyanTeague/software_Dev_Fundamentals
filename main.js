def44f222ae81c3d8e436af623da1ab385f8d478
RyanTeague <Qews_3124>
2024-10-26 15:23:32 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var cars = ["fiat", "beenz", "honda", "toyota"]
+
+
+
+for( let i = 0; i < cars.length; i++) {
+    if(cars[i] = "beenz") {
+        console.log("you're driving a sweet" + cars[i])
+    }
+    console.log(cars[i])
+    
+}
